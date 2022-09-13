e535e7071f1e2fdc86cf7f3526d603f0d975fa18
yosua2468 <111826210+yosua2468@users.noreply.github.com>
2022-09-13 11:11:07 +0700

    buat view biding

---
+++ app/src/main/java/com/example/view_binding_10367/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.view_binding_10367
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.view_binding_10367.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val adapter = MainAdapter(TaskList.taskList)
+        binding?.taskRv?.adapter = adapter
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+}
